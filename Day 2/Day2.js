030644361f8ce53255685df344bef8b2d19cbcdf
Sushil <kidsushilink@gmail.com>
2024-02-07 14:19:34 -0800

    Day 2
    operators

---
+++ Day 2/Day2.js
@@ -0,0 +1,11 @@
+// operator
+
+let a = 5;
+let b = 2;
+console.log("a = ",a+ " b = ",b);
+console.log("plus",a+b);
+console.log("minus", a-b);
+console.log("multiply", a*b);
+console.log("divide", a/b);
+console.log(a % b);
+console.log(a ** b);
