b573d69c72dbbb3a8f2a62da0e562e2348a44220
Sushil <kidsushilink@gmail.com>
2024-02-08 00:29:09 -0800

    update

--- Day 3/pract.js
+++ Day 3/pract.js
@@ -13,7 +13,7 @@ for (let i = 1; i<=100; i++){
 
 
 //2
- 
+// random guessing number
 let ranNum = 5;
 let userInput = prompt("Welcome to guessing game\nEnter number between [1-10]\nEnter your Number: ")
 
@@ -21,3 +21,54 @@ while (ranNum != userInput){
     userInput = prompt("You Entered Wrong Number\nWelcome to guessing game\nEnter number between [1-10]\nEnter your Number: ")
 }
 console.log("Wow, You guessed correct number!!");
+
+
+
+// string
+
+let naam = "Sushil Thapa from nepal";
+
+console.log(naam.length)
+
+console.log(naam[4])
+
+
+// template literals
+
+let specialString = `this is template literal`;
+
+console.log(specialString);
+
+
+let obj = {
+    item: "personalbr",
+    price: 20,
+}
+
+console.log(`The cost of ${obj.item} is ${obj.price}`);
+
+
+
+
+// string methods in js
+
+let nam1 = "            SuShIl THApa";
+let nam = " hello";
+console.log(nam.toUpperCase());
+console.log(nam.toLowerCase());
+console.log(nam.trim());
+console.log(nam.slice(3,7));
+console.log(nam.concat(nam1));
+
+console.log(nam);
+
+
+// 3
+
+let userInp = prompt("Enter your full name: ");
+let user1 = "@";
+
+op = user1.concat(userInp);
+len = userInp.length;
+
+console.log(op+len);
