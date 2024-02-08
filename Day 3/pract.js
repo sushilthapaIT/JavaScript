c59ff432d07a20ddf7712f892d91951c3629a92f
Sushil <kidsushilink@gmail.com>
2024-02-07 21:42:59 -0800

    remove comment

--- Day 3/pract.js
+++ Day 3/pract.js
@@ -1,15 +1,15 @@
 // practice loop
 
 // 1
-// to print even number from 1-100
+//to print even number from 1-100
 
-// for (let i = 1; i<=100; i++){
+for (let i = 1; i<=100; i++){
     
-//     if (i%2 === 0){
-//         console.log("The even number between 1 to 100 are:",i);
-//     }
+    if (i%2 === 0){
+        console.log("The even number between 1 to 100 are:",i);
+    }
 
-// } 
+} 
 
 
 //2
