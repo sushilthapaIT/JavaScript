22946f728af276365deee0c17cce83c9ee010372
Sushil <kidsushilink@gmail.com>
2024-02-09 01:57:28 -0800

    function
    method map, reduce, filter, arrow

---
+++ Chapter 5 (Functions)/method array/script.js
@@ -0,0 +1,86 @@
+function welcome() {
+    console.log("Welcome to my program\n--------------------------")
+}
+
+function myFunc(welcome){
+    return welcome;
+}
+
+
+let arr = [1,2,3,4,5,6];
+
+arr.forEach(function printVal(val, i ) {
+    console.log(val, i, arr);
+});
+
+
+let arr = [2,3,1,3,5,6,7,8,9,7]
+
+arr.forEach((val) => {
+    console.log(`square of ${val} = ${val*val}`);
+});
+
+
+
+// map function
+// return new array 
+
+let arrs = [2,3,1,3,5,6,7,8,9,7]
+arrs.map((val) => {
+    return
+});
+
+
+// filter array method
+
+let naw = [ 1,2,3,4,5,6,7,7,8,9,0];
+
+let naya = naw.filter((val) => {
+    return val%2 == 0;
+});
+
+console.log(naya);
+
+
+// reduce array method
+
+let okn  = [2,3,4,5,6,7,8,9];
+
+let outp = okn.reduce((res, curr) => {
+    return res + curr;
+})
+
+console.log(outp);
+
+
+q1
+let marks = [22,98,97,87,99,89];
+
+let filMarks = marks.filter((val) => {
+    return val > 90;
+})
+
+console.log(filMarks);
+
+q2
+let userInput = prompt("Enter how many number of list you want to create: ");
+let newArray = [];
+
+    for (let i = 0; i < userInput; i++){
+        let inp = prompt(`Enter ${i+1} number: `);
+        newArray.push(inp);
+    }
+    console.log(newArray);
+
+let sumArray = newArray.reduce((prev, curr) => {
+    return prev + curr;
+});
+
+
+
+const productArray = newArray.reduce((prev, curr){
+    return prev * curr;
+})
+
+console.log(sumArray);
+console.log(productArray);
--- Chapter 5 (Functions)/script.js
+++ Chapter 5 (Functions)/script.js
@@ -1,3 +1,141 @@
 // function can be invoked anytime whenever needed
 
-console.log("Hello World!!!")
+// console.log("Hello World!!!")
+
+
+// to make function
+
+// function firstFunction(){
+
+// }
+
+
+// function myFunction(){
+//     console.log("Welcome to Lambton College.\nI am learning JS Function.")
+// }
+
+
+// myFunction();
+// myFunction();
+
+
+// function myFunction(msg){ // parameter
+//     console.log(msg)
+// }
+
+
+// myFunction("This is Sushil");  // argument
+// myFunction("I am Batman");
+
+
+
+
+// function mySum (a, b){
+//     console.log(a + b);
+// }
+
+// mySum(99, 1);
+
+
+
+// function myX(a, b){
+//     s = a + b ;
+//     return s;
+// }
+
+// l = myX(8, 8);
+
+// console.log(l);
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// function mul(a, b){
+//     return a * b;
+// }
+
+// arrow function
+// function sum(a, b){
+//     return a + b;
+// }
+
+
+// sum function in arrow function
+const arrowSum = (a, b) => {
+    console.log(a+b);
+}
+
+arrowSum(2, 2);
+
+
+const arrowMul = (a, b) => {
+    console.log(a*b);
+}
+
+arrowMul(2, 2);
+
+
+let stri = "Romika".toString().toLowerCase();
+// new = stri.
+let li = ["a","e","i","o","u"].toString();
+let count = 0;
+for (let i = 0; i < stri.length; i++){
+    if ((li).includes(stri[i])){
+        count = count + 1;
+    }
+    // console.log(li);
+
+    // if (stri[i] === li[i]){
+
+    // }
+}
+
+console.log(count);
+
+
+const incLude = (cout) => {
+    let stri = "Romika".toString().toLowerCase();
+// new = stri.
+    let li = ["a","e","i","o","u"].toString();
+    let count = 0;
+    for (let i = 0; i < stri.length; i++){
+        if ((li).includes(stri[i])){
+            count = count + 1;
+        }
+        // console.log(li);
+
+        // if (stri[i] === li[i]){
+
+        // }
+    }
+        return count;
+    }
+
+
+result = incLude();
+
+console.log("The numnber of vovwel in Romika is ", result);
+
+
+function ret(rest){
+    let stri = "Romika".toString().toLowerCase();
+    // new = stri.
+    let li = ["a","e","i","o","u"].toString();
+    let count = 0;
+    for (let i = 0; i < stri.length; i++){
+        if ((li).includes(stri[i])){
+            count = count + 1;
+        }
+        // console.log(li);
+
+        // if (stri[i] === li[i]){
+
+        // }
+    }
+    return count;
+}
+
+
+result = ret();
+console.log(result);
