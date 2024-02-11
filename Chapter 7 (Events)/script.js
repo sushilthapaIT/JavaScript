0902839119536b9488a22156452bc197d7e1df64
Sushil <kidsushilink@gmail.com>
2024-02-11 15:17:58 -0800

    Complete Basic Event Handler
    - made rock, paper, scissor game
    -- learn how to handle event

--- Chapter 7 (Events)/script.js
+++ Chapter 7 (Events)/script.js
@@ -42,18 +42,98 @@
 
 ////assignment
 
-let modeBtn = document.querySelector("#mode");
-let currMode = "light"; 
+// let modeBtn = document.querySelector("#mode");
+// let currMode = "light"; 
 
-modeBtn.addEventListener("click", () => {
+// modeBtn.addEventListener("click", () => {
+//     if (currMode === "light"){
+//         currMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "black";
+//     }
+//     else{
+//         currMode = "light";
+//         document.querySelector("body").style.backgroundColor = "white";
+
+//     }
+//     console.log(currMode);
+// })
+
+
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.addEventListener("click", () => {
+//     console.log("Button clicked - handler 1")
+// })
+
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (e) => {
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+
+//     console.log("btn1 was clicked");
+// }
+
+
+// const handler3 =() => {
+//     console.log("button was clicked 3")
+// };
+
+// btn1.addEventListener("click", () => {
+//     console.log("button was clicked")
+// })
+
+// btn1.addEventListener("click", () => {
+//     console.log("button was clicked 2")
+// })
+
+// btn1.addEventListener("click", handler3);
+
+
+
+// btn1.addEventListener("click", () => {
+//     console.log("button was clicked 4")
+// })
+
+// btn1.removeEventListener("click", handler3);
+
+
+// let div = document.querySelector("div");
+
+// div.onmouseover = () => {
+//     console.log("Your are inside div")
+// }
+
+// let modeB = document.querySelector("#mode");
+// let currMode = "light";
+
+// modeB.addEventListener("click", () => {
+//     if (currMode === "light"){
+//         currMode = "dark";
+//     }
+//     else {
+//         currMode = "light";
+//     }
+// });
+
+
+let modeB = document.querySelector("#mode");
+let currMode = "light";
+let body = document.querySelector("body");
+
+modeB.addEventListener("click", () => {
     if (currMode === "light"){
         currMode = "dark";
-        document.querySelector("body").style.backgroundColor = "black";
+        body.classList.add("dark");
+        body.classList.remove("light");
+
     }
-    else{
+    else {
         currMode = "light";
-        document.querySelector("body").style.backgroundColor = "white";
+        body.classList.add("light");
+        body.classList.remove("dark");
 
     }
     console.log(currMode);
-})
+});
